4a543478124e6215d60d8ccab80648f6ece5864e
Henry <henry.ik@hotmail.com>
2017-07-10 15:04:22 +0800

    initial commit: navbar, article, index

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article Un',
+            'body':'lorem ipsum dolor sit amet, consectetur adipisicing elit. impedit ipsa, laborum ma',
+            'author':'Henri Tai',
+            'create_date':'10-07-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Duex',
+            'body': 'lorem ipsum dolor sit amet, consectetur adipisic fuga fugiat impedit ipsa, laborum ma',
+            'author': 'John doe',
+            'create_date': '10-07-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Troi',
+            'body': 'lorem   ipsum dolor sit amet, consectetur adieliemque fuga fugiat impedit ipsa, laborum ma',
+            'author': 'Jane Doe',
+            'create_date': '10-07-2017'
+        }
+    ]
+    return articles
